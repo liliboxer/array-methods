0e46feda216bc467ecb010564737299683b142c8
Lili Boxer <ninaliboxer@gmail.com>
2019-06-25 09:52:58 -0700

    test set up for reduce

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-const numbers = [1, 2, 3];
-
 function map(arr, callback) {
   const mapped = [];
   for(let i = 0; i < arr.length; i++) {
     mapped[i] = callback(arr[i]);
-    // console.log('mapped', callback(arr[i]));
   }
   return mapped;
 }
-
-const double = map(numbers, arr => arr * 2);
-// console.log(double);
-
-///
-
-
-const test = ['max', 'orangetabby', 'cat', 'meow', '8', 'maxwell'];
 
 function filter(arr, callback) {
   const filtered = [];
   for(let i = 0; i < arr.length; i++) {
     if(callback(arr[i])) {
       filtered[filtered.length] = arr[i];
-      console.log('length', filtered.length);
-      console.log('i', i);
 
     }
   }
   return filtered;
 }
 
-const filtered = filter(test, arr => arr.length < 4);
-console.log('hi', filtered);
+function reduce(arr, callback) {
+  const reduced = [];
+  for(let i = 0; i < arr.length; i++) {
+    reduced[i] === callback(arr);
+  }
+  return reduced;
+}
+
+
 
 module.exports = {
   map,
-  filter
+  filter,
+  reduce
 };
 
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { map, filter } = require('./index');
+const { map, filter, reduce } = require('./index');
 
 describe('array-methods', () => {
   it('doubles array', () => {
@@ -11,6 +11,12 @@ describe('array-methods', () => {
     const arr = ['max', 'orangetabby', 'cat', 'meow', '8', 'maxwell'];
     const filtered = filter(arr, arr => arr.length < 4);
     expect(filtered).toEqual(['max', 'cat', '8']);
+  });
+
+  it('add numbers together', () => {
+    const arr = [2, 3, 4];
+    const reduced = reduce(arr, arr => arr += arr);
+    expect(reduced).toEqual([9]);
   });
 
 });
