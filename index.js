f623c5646294bbef3fc201a58ccba762080d6cbc
Lili Boxer <ninaliboxer@gmail.com>
2019-06-24 21:43:09 -0700

    things t/f

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
+const numbers = [1, 2, 3];
 
 function map(arr, callback) {
   const mapped = [];
   for(let i = 0; i < arr.length; i++) {
     mapped[i] = callback(arr[i]);
+    // console.log('mapped', callback(arr[i]));
   }
   return mapped;
-};
+}
+
+const double = map(numbers, arr => arr * 2);
+// console.log(double);
+
+///
+
+
+const test = ['max', 'orange', 'budd', 'black'];
 
 function filter(arr, callback) {
   const filtered = [];
-  for (let i = 0; i < arr.length; i++) {
-  
-    
+  for(let i = 0; i < arr.length; i++) {
+    filtered[i] = callback(arr[i]);
+    console.log('filtered', callback(arr[i]));
   }
+  return filtered;
 }
+
+const filtered = filter(test, arr => arr.length < 4);
+console.log(filtered);
 
 module.exports = {
   map,
   filter
-}
+};
 
--- index.test.js
+++ index.test.js
@@ -1,15 +1,16 @@
-const { map } = require('./index');
+const { map, filter } = require('./index');
 
 describe('array-methods', () => {
-
   it('doubles array', () => {
     const arr = [1, 2, 3];
     const double = map(arr, arr => arr * 2);
     expect(double).toEqual([2, 4, 6]);
   });
 
-  it('filters array', () => {
-
+  it('includes orange', () => {
+    const arr = ['max', 'orange'];
+    const filtered = filter(arr, arr => arr.length < 4);
+    expect(filtered).toEqual(arr);
   });
 
 });
